6b37dac04cc2863abe18d0545f456d30942effce
Aita K <67955300+EyelidSilver@users.noreply.github.com>
2021-01-21 16:53:25 +0100

    Add files via upload

---
+++ StickerShop/script.js
@@ -0,0 +1,11 @@
+
+      function visitBehance() {
+          window.open('https://www.behance.net/ayta19516bac');
+      }
+
+
+      function visitWorkPage() {
+          window.open('work.html');
+      }
+
+      
